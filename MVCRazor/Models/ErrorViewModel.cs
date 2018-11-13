7f2fc60739aa923592c9f0dc814a80fbe16879ca
Matthew Webb <42815501+MJW3BB@users.noreply.github.com>
2018-11-13 16:05:42 -0600

    Razor Templates have been added
    Mission Complete // MW

---
+++ MVCRazor/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCRazor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
