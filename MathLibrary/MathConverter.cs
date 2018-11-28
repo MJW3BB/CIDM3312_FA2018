f4c5dccb1f6febcfffbf6b39ae75f0818b927c0e
Matthew Webb <42815501+MJW3BB@users.noreply.github.com>
2018-11-28 12:11:43 -0600

    Added Library for Math
    Be sure to build and add .dll to the root of C:\

---
+++ MathLibrary/MathConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MathLibrary
+{
+    public class MathConverter
+    {
+        public static decimal Add(decimal left, decimal right)
+        {
+            return left + right;
+        }
+        public static decimal Subtract(decimal left, decimal right)
+        {
+            return left - right;
+        }
+        public static decimal Multiply(decimal left, decimal right)
+        {
+            return left * right;
+        }
+        public static decimal Divide(decimal left, decimal right)
+        {
+            return left / right;
+        }
+        public static decimal Modulus(decimal left, decimal right)
+        {
+            return left % right;
+        }
+        public static double Power(double left, double right)
+        {
+            return Math.Pow(left, right);
+        }
+        public static decimal Floor(decimal number)
+        {
+            return Math.Floor(number);
+        }
+        public static decimal Ceiling(decimal number)
+        {
+            return Math.Ceiling(number);
+        }
+    }
+}
